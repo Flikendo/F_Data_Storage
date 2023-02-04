0901f1c9368f44d59cafdc133ad481911f460ba1
Flikendo <jalasalavidal@gmail.com>
2023-02-04 17:03:20 +0100

    Importing project to Git

---
+++ src/main/java/com/flikendo/F_Data_Storage/FDataStorageApplication.java
@@ -0,0 +1,13 @@
+package com.flikendo.F_Data_Storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FDataStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FDataStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flikendo/F_Data_Storage/FDataStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flikendo.F_Data_Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FDataStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
