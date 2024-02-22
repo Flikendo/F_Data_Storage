31cb25cd35279ed644420eee7cbabef3135ee6d4
Flikendo <jalasalavidal@gmail.com>
2024-02-22 21:00:17 +0100

    Kafka consumer configuration

---
+++ src/main/java/com/flikendo/datastorage/configuration/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,53 @@
+package com.flikendo.datastorage.configuration.kafka;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
+
+/**
+ * Date: 02-21-2024
+ * Author: Flikendo
+ * KafkaConsumerConfig class. This class configures Kafka consumer
+ */
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+    // Localhost:port read from application.properties
+    @Value("${spring.kafka.bootstrapServers}")
+    private String bootstrapServers;
+
+    // Group Id read from application.conf
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    /**
+     * Creates consumer factory object
+     * @return a ConsumerFactory<String, Object> which contains the whole configuration
+     */
+    @Bean
+    public ConsumerFactory<String, String> createConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(GROUP_ID_CONFIG, groupId);
+        props.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(createConsumerFactory());
+        return factory;
+    }
+}
